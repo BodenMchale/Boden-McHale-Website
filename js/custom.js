ea9db509fadcf57ad3bb0485a5367cb1c3c4c06c
David McHale <dmchale92@gmail.com>
2015-05-21 15:24:23 -0400

    Updating Homepage Styling

--- js/custom.js
+++ js/custom.js
@@ -1,134 +1,151 @@
-jQuery(document).ready(function(){
-	tfuse_custom_form();
-    loginFormValidator();
+/* ========================================================================= */
+/*	Preloader
+/* ========================================================================= */
+
+jQuery(window).load(function(){
+
+	$("#preloader").fadeOut("slow");
+
 });
 
-function tfuse_custom_form(){
-	var my_error;
-	var url = jQuery("input[name=temp_url]").attr('value');
-	jQuery("#send").bind("click", function(){
 
-	my_error = false;
-	jQuery("#commentForm input, #commentForm textarea, #commentForm select").each(function(i)
-	{
-				var surrounding_element = jQuery(this);
-				var value 				= jQuery(this).val();
-				var check_for 			= jQuery(this).attr("id");
-				var required 			= jQuery(this).hasClass("required");
+$(document).ready(function(){
 
-				if(check_for == "email"){
-					surrounding_element.removeClass("error valid");
-					baseclases = surrounding_element.attr("class");
-					if(!value.match(/^\w[\w|\.|\-]+@\w[\w|\.|\-]+\.[a-zA-Z]{2,4}$/)){
-						surrounding_element.attr("class",baseclases).addClass("error");
-						my_error = true;
-					}else{
-						surrounding_element.attr("class",baseclases).addClass("valid");
-					}
-				}
+	/* ========================================================================= */
+	/*	Menu item highlighting
+	/* ========================================================================= */
 
-				if(check_for == "message"){
-					surrounding_element.removeClass("error valid");
-					baseclases = surrounding_element.attr("class");
-					if(value == "" || value == "Hi how are you?"){
-						surrounding_element.attr("class",baseclases).addClass("error");
-						my_error = true;
-					}else{
-						surrounding_element.attr("class",baseclases).addClass("valid");
-					}
-				}
-
-				if(required && check_for != "email" && check_for != "message"){
-					surrounding_element.removeClass("error valid");
-					baseclases = surrounding_element.attr("class");
-					if(value == ""){
-						surrounding_element.attr("class",baseclases).addClass("error");
-						my_error = true;
-					}else{
-						surrounding_element.attr("class",baseclases).addClass("valid");
-					}
-				}
-
-
-			   if(jQuery("#commentForm input,#commentForm textarea,#commentForm select").length  == i+1){
-					if(my_error == false){
-						document.commentForm.submit();
-                        /*
-                        jQuery(".ajax_form").slideUp(400);
-
-						var $datastring = "ajax=true";
-						jQuery(".ajax_form input, .ajax_form select").each(function(i)
-						{
-							var $name = jQuery(this).attr('name');
-							var $value = encodeURIComponent(jQuery(this).attr('value'));
-							$datastring = $datastring + "&" + $name + "=" + $value;
-						});
-
-
-						jQuery(".ajax_form #send").fadeOut(100);
-
-						jQuery.ajax({
-						   type: "POST",
-						   url: "./sendmail.php",
-						   data: $datastring,
-						   success: function(response){
-						   jQuery(".ajax_form").before("<div class='ajaxresponse' style='display: none;'></div>");
-						   jQuery(".ajaxresponse").html(response).slideDown(400);
-						   jQuery(".ajax_form #send").fadeIn(400);
-						   jQuery(".ajax_form input, .ajax_form textarea, .ajax_form radio, .ajax_form select").val("");
-							   }
-							});
-						*/
-						}
-				}
-
-			});
-			return false;
+	jQuery('#nav').singlePageNav({
+		offset: jQuery('#nav').outerHeight(),
+		filter: ':not(.external)',
+		speed: 1200,
+		currentClass: 'current',
+		easing: 'easeInOutExpo',
+		updateHash: true,
+		beforeStart: function() {
+			console.log('begin scrolling');
+		},
+		onComplete: function() {
+			console.log('done scrolling');
+		}
 	});
-}
-
-function loginFormValidator(){
-    var login_error;
-    var loginform = jQuery('#loginform');
-    jQuery("#login-submit").bind("click", function(){
-
-        login_error = false;
-        jQuery("#loginform input").each(function(i)
-        {
-            var surrounding_element = jQuery(this);
-            var value 				= jQuery(this).val();
-            var check_for 			= jQuery(this).attr("id");
-            var required 			= jQuery(this).hasClass("required");
-
-            if(check_for == "user_login"){
-                surrounding_element.parent().removeClass("error valid");
-                baseclases = surrounding_element.attr("class");
-                if(!value.match(/^\w[\w|\.|\-]+@\w[\w|\.|\-]+\.[a-zA-Z]{2,4}$/)){
-                    surrounding_element.parent().addClass("error");
-                    login_error = true;
-                }else{
-                    surrounding_element.parent().addClass("valid");
-                }
-            }
-
-            if(required && check_for != "user_login"){
-                surrounding_element.parent().removeClass("error valid");
-                baseclases = surrounding_element.attr("class");
-                if(value.length <= 5){
-                    surrounding_element.parent().addClass("error");
-                    login_error = true;
-                }else{
-                    surrounding_element.parent().addClass("valid");
-                }
-            }
-
-            if(jQuery("#loginform input").length  == i+1){
-                if(login_error == false){
-                    loginform.submit();
-                }
-            }
-
-        });
-        return false;
+	
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 400) {
+            $("#navigation").css("background-color","#0EB493");
+        } else {
+            $("#navigation").css("background-color","rgba(16, 22, 54, 0.2)");
+        }
     });
+	
+	/* ========================================================================= */
+	/*	Fix Slider Height
+	/* ========================================================================= */	
+
+	var slideHeight = $(window).height();
+	
+	$('#slider, .carousel.slide, .carousel-inner, .carousel-inner .item').css('height',slideHeight);
+
+	$(window).resize(function(){'use strict',
+		$('#slider, .carousel.slide, .carousel-inner, .carousel-inner .item').css('height',slideHeight);
+	});
+	
+	
+	/* ========================================================================= */
+	/*	Portfolio Filtering
+	/* ========================================================================= */	
+	
+	
+    // portfolio filtering
+
+    $(".project-wrapper").mixItUp();
+	
+	
+	$(".fancybox").fancybox({
+		padding: 0,
+
+		openEffect : 'elastic',
+		openSpeed  : 650,
+
+		closeEffect : 'elastic',
+		closeSpeed  : 550,
+
+		closeClick : true,
+	});
+	
+	/* ========================================================================= */
+	/*	Parallax
+	/* ========================================================================= */	
+	
+	$('#facts').parallax("50%", 0.3);
+	
+	/* ========================================================================= */
+	/*	Timer count
+	/* ========================================================================= */
+
+	"use strict";
+    $(".number-counters").appear(function () {
+        $(".number-counters [data-to]").each(function () {
+            var e = $(this).attr("data-to");
+            $(this).delay(6e3).countTo({
+                from: 50,
+                to: e,
+                speed: 3e3,
+                refreshInterval: 50
+            })
+        })
+    });
+	
+	/* ========================================================================= */
+	/*	Back to Top
+	/* ========================================================================= */
+	
+	
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 400) {
+            $("#back-top").fadeIn(200)
+        } else {
+            $("#back-top").fadeOut(200)
+        }
+    });
+    $("#back-top").click(function () {
+        $("html, body").stop().animate({
+            scrollTop: 0
+        }, 1500, "easeInOutExpo")
+    });
+	
+});
+
+
+// ==========  START GOOGLE MAP ========== //
+function initialize() {
+    var myLatLng = new google.maps.LatLng(22.402789, 91.822156);
+
+    var mapOptions = {
+        zoom: 14,
+        center: myLatLng,
+        disableDefaultUI: true,
+        scrollwheel: false,
+        navigationControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        draggable: false,
+        mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'roadatlas']
+        }
+    };
+
+    var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        icon: 'img/location-icon.png',
+        title: '',
+    });
+
 }
+
+google.maps.event.addDomListener(window, "load", initialize);
+// ========== END GOOGLE MAP ========== //
